823ca8dd06cc6f3e430dd9d01aad062458dc8f37
Harshith <harshithlokineni@gmail.com>
2025-07-05 10:43:54 +0530

    CHANGE APP NAME

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+            HARSHITH URL SHORTNER
           </Title>
 
           <Text
